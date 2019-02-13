952eca8d152158677d40fe9881a3deedb178a987
afaraone <andresfaraone@outlook.com>
2019-02-13 22:45:46 +0000

    Create mock html for individual event page and set up mocking

---
+++ spec/parse_event_page_spec.rb
@@ -0,0 +1,9 @@
+require 'parse_event_page'
+
+describe ParseEventPage do
+  #set up mock html file 
+  subject { described_class.new('./spec/mock-event-page.html') }
+
+  describe '#extract date' do
+  end
+end
