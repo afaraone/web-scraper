2d5eb7d87469ece77efcdce280ea49b568a4a466
afaraone <andresfaraone@outlook.com>
2019-02-14 00:01:05 +0000

    made #extract_url and #get_event private
    return_json is the main interface for class, outputs JSON string
    of all event hashes

--- lib/parse_event_index.rb
+++ lib/parse_event_index.rb
@@ -1,13 +1,21 @@
 require 'nokogiri'
 require 'open-uri'
 require_relative 'parse_event_page.rb'
+require 'json'
 
 class ParseEventIndex
   attr_reader :html
 
   def initialize(url)
     @html = Nokogiri::HTML(open(url))
+    @event_array = get_event_hashes(extract_event_urls)
   end
+
+  def return_json
+    @event_array.to_json
+  end
+
+  private
 
   def get_event_hashes(url_array)
     url_array.map do |event_url|
